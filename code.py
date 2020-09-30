c146995f9ad9bdf9ad756ffae7103d89d6ce12d8
Joi <37125335+joicodes@users.noreply.github.com>
2020-09-30 12:18:03 -0400

    Update code.py

--- code.py
+++ code.py
@@ -11,6 +11,7 @@ https://github.com/adafruit/Adafruit_CircuitPython_CircuitPlayground/tree/master
 # Import CPX Library
 from adafruit_circuitplayground.express import cpx
 from time import sleep
+from random import choice
 
 # The RGB values:
 BLACK   = (0,0,0)
@@ -22,31 +23,82 @@ CYAN    = (0,255,255)
 GREEN   = (0,255,0)
 YELLOW  = (255,255,0) 
 
+color_list = [RED, WHITE, MAGENTA, BLUE, CYAN, GREEN, YELLOW]
 
-def change_pixel(index, color):
-    '''Changes the RGB color of a given NeoPixel.'''
-    cpx.pixel[index] = color
 
-def fill_pixels(color):
-    '''Fills all NeoPixel with given color.'''
-    cpx.pixels.fill(color)
-
-def lights_out():
-    '''Fills all NeoPixels black.'''
-    fill_pixels(BLACK)
+#💡 NeoPixels - 10 built in 10 RGB NeoPixel LEDs.
+# Colors are set in the form of an (R, G, B) tuple. 
+# NeoPixel Helper Functions (use cpx.pixels):
+def change_color(color, index=None):
+    '''
+    Changes the RGB color of a given NeoPixel. \n
+    If index exceeds number of pixels, changes all pixels to given color.
+    If index is not passed, changes all pixels to given color. 
+    '''
+    if index == None or index >= len(cpx.pixels):
+        cpx.pixels.fill(color)
+    else:
+        cpx.pixels[index] = color
 
 def blink_color(color, time=0.5):
     '''Flashes a given color, for a given number of seconds'''
-    fill_pixels(color)
+    change_color(color)
     sleep(time)
-    lights_out()
+    change_color()
     sleep(time)
+
+
+#🚨 D13 - red LED (D13) next to the USB port.
+# 'on' and 'off' are states are True and False.
+# D13 Helper Functions (Use cpx.red_led):
+def blink_red_led(time=0.5):
+    '''Flashes a given color, for a given number of seconds'''
+    cpx.red_led = True
+    sleep(time)
+    cpx.red_led = False
+    sleep(time)
+
+
+#🎚 D7 - side swich above the battery connector.
+# 'on' and 'off' are states are True and False.
+# D7 Helper Functions (Use cpx.red_led):
+
+
+# Built in accelerometer functionality:
+
+#👆 Tap Detection - options: single tap, double tap
+# Tap Helper Functions (Use cpx.tapped)
+
+# Example: cpx.tapped
+
+
+#🤝 Shake Detection - options: shake_threshold (int)
+# Default Threshold:shake_threshold = 30
+# Lowest (more sensitive): shake_threshold = 10
+# Shake Helper Functions (Use cpx.shake())
+
+# Example: cpx.shake(shake_threshold=20)
+
+#TODO: https://learn.adafruit.com/circuitpython-made-easy-on-circuit-playground-express/light
+#🌞 A8 Light Detection - light sensor
+# Default Threshold:shake_threshold = 30
+# Lowest (more sensitive): shake_threshold = 10
+# Shake Helper Functions (Use cpx.shake())
+
+# Example: cpx.shake(shake_threshold=20)
+
+
+
+# Initialize Settings:
+cpx.pixels.brightness = 0.1
+cpx.detect_taps = 1  # Pre-set for double tap detection
+on = True
 
 while True:
 
-    on = True
+    # D13 on when switch is on.
+    cpx.red_led = cpx.switch
 
-    if cpx.switch == on:
-        lights_out()
-    else:
-        fill_pixels(BLACK)
+    if cpx.tapped:
+        current_color = choice(color_list)
+        change_color(current_color)
